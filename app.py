9dfa56f8caf24c1a5119bc7bc0d5de0aa6a2b7c7
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-11-17 14:17:27 +0530

    Landing Page created

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Personal Expences Tracker</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
