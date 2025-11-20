46530dd2e46debe8505ceb3145c56278e5c34a2c
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-11-20 20:25:14 +0530

    signin and signup page created

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/SignIn')
+def signin():
+    return render_template("signin.html")
+
+@app.route('/SignUp')
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
